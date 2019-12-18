0212305d9c52c4427f9b2abad6e8db2205f1689e
Dan Kelosky <dkelosky@gmail.com>
2019-12-17 20:09:53 -0500

    definition

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,17 +13,14 @@ import {
     SymbolInformation,
     TextDocument,
     Diagnostic,
-    DiagnosticSeverity
+    DiagnosticSeverity,
+    Location
 } from "vscode-languageserver";
 
 const MAX_LEN = 80;
 
-// Create a connection for the server. The connection uses Node's IPC as a transport.
-// Also include all preview / proposed LSP features.
 const connection = createConnection(ProposedFeatures.all);
 
-// Create a simple text document manager. The text document manager
-// supports full document sync only
 const documents: TextDocuments = new TextDocuments();
 
 let hasConfigurationCapability: boolean = false;
@@ -33,8 +30,6 @@ let hasDiagnosticRelatedInformationCapability: boolean = false;
 connection.onInitialize((params: InitializeParams) => {
     const capabilities = params.capabilities;
 
-    // Does the client support the `workspace/configuration` request?
-    // If not, we will fall back using global settings
     hasConfigurationCapability = !!(
         capabilities.workspace && !!capabilities.workspace.configuration
     );
@@ -51,13 +46,13 @@ connection.onInitialize((params: InitializeParams) => {
         capabilities: {
             textDocumentSync: documents.syncKind,
             documentSymbolProvider: true,
+            definitionProvider: true,
         }
     };
 });
 
 connection.onInitialized(() => {
     if (hasConfigurationCapability) {
-        // Register for all configuration changes.
         connection.client.register(DidChangeConfigurationNotification.type, undefined);
     }
     if (hasWorkspaceFolderCapability) {
@@ -67,19 +62,14 @@ connection.onInitialized(() => {
     }
 });
 
-// The example settings
 interface IHlasmSettings {
     maxNumberOfProblems: number;
 }
 
-// The global settings, used when the `workspace/configuration` request is not supported by the client.
-// Please note that this is not the case when using this server with the client provided in this example
-// but could happen with other clients.
 const defaultSettings: IHlasmSettings = { maxNumberOfProblems: 1000 };
 let globalSettings: IHlasmSettings = defaultSettings;
-
-// Cache the settings of all open documents
 const documentSettings: Map<string, Thenable<IHlasmSettings>> = new Map();
+const symbolsCache: Map<string, Location[]> = new Map();
 
 connection.onDidChangeConfiguration((change) => {
     if (hasConfigurationCapability) {
@@ -110,29 +100,24 @@ function getDocumentSettings(resource: string): Thenable<IHlasmSettings> {
 }
 
 // Only keep settings for open documents
-documents.onDidClose(e => {
+documents.onDidClose((e) => {
     documentSettings.delete(e.document.uri);
 });
 
-// The content of a text document has changed. This event is emitted
-// when the text document first opened or when its content has changed.
 documents.onDidChangeContent((change) => {
     validateTextDocument(change.document);
 });
 
 async function validateTextDocument(textDocument: TextDocument): Promise<void> {
-    // In this simple example we get the settings for every validate run.
-    const settings = await getDocumentSettings(textDocument.uri);
 
-    // The validator creates diagnostics for all uppercase words length 2 and more
+    const settings = await getDocumentSettings(textDocument.uri);
+    connection.console.info(`validateTextDocument() called - settings are: ${JSON.stringify(settings, null, 2)}`);
+
     const text = textDocument.getText();
 
+    // TODO(Kelosky): stay within settings threshold
     let problems = 0;
     const diagnostics: Diagnostic[] = [];
-
-    // TODO(Kelosky): account for max number of problems
-    //   while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
-    // TODO(Kelosky): print diagnostics for blank lines
 
     const lines = text.split("\n");
     for (let i = 0; i < lines.length - 1; i++) {
@@ -148,68 +133,127 @@ async function validateTextDocument(textDocument: TextDocument): Promise<void> {
                 message: `Exceeded ${MAX_LEN} characters`,
                 source: "hlasm"
             };
-            // if (hasDiagnosticRelatedInformationCapability) {
-            //     diagnostic.relatedInformation = [
-            //         {
-            //             location: {
-            //                 uri: textDocument.uri,
-            //                 range: Object.assign({}, diagnostic.range)
-            //             },
-            //             message: 'Spelling matters'
-            //         },
-            //         {
-            //             location: {
-            //                 uri: textDocument.uri,
-            //                 range: Object.assign({}, diagnostic.range)
-            //             },
-            //             message: 'Particularly for names'
-            //         }
-            //     ];
-            // }
+
             diagnostics.push(diagnostic);
         }
-
-        // const tokenizedLine = lines[i].replace(/\s+/g, " ").split(" ");
-
-        // if (tokenizedLine.length > 0) {
-
-        //     let kind: SymbolKind = SymbolKind.Constant;
-
-        //     // TODO(Kelosky): if LABEL MVC -> this fails to capture
-        //     if (tokenizedLine[0]) {
-        //         if (tokenizedLine[0] === "MVC") {
-        //             kind = SymbolKind.Object;
-        //         }
-
-        //         if (!tokenizedLine[1]) {
-        //             // error missing arguments
-        //             const start = lines[i].indexOf(tokenizedLine[0]);
-        //             const diagnostic: Diagnostic = {
-        //                 severity: DiagnosticSeverity.Error,
-        //                 range: {
-        //                     start: { line: i, character: start },
-        //                     end: { line: i, character: start + tokenizedLine[0].length },
-        //                 },
-        //                 message: `Argument(s) missing for ${tokenizedLine[0]}`,
-        //                 source: "hlasm"
-        //             };
-        //         }
-        //     }
-        // }
-
     }
 
-    // Send the computed diagnostics to VSCode.
     connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
 }
 
+connection.onDefinition((parm) => {
+    connection.console.info(`onDefinition invoked ${JSON.stringify(parm, null, 2)}`);
+
+    const document = documents.get(parm.textDocument.uri);
+    if (!document) {
+        return null;
+    }
+
+    const lines = document.getText().split("\n");
+
+    connection.console.info(`Definition ${parm.position.character} on line '${lines[parm.position.line]}'`);
+    connection.console.info(`There are ${symbolsCache.size} entries in cached symbols`);
+
+    // if (lines[parm.position.line].trim() === "") {
+    //     connection.console.info(`No definition for blank line`);
+    //     return;
+    // }
+
+    // if (parm.position.character === 0) {
+    //     connection.console.info(`No definition for something in column 0`);
+    //     return;
+    // }
+
+    // if (lines[parm.position.line][parm.position.character] === " ") {
+    //     connection.console.info(`No definition for spaces`);
+    //     return;
+    // }
+
+    // if (lines[parm.position.line][parm.position.character] === "*") {
+    //     connection.console.info(`No definition for asterisks`);
+    //     return;
+    // }
+
+    const len = lines[parm.position.line].length;
+    const reg = new RegExp(/[a-z|A-Z|0-9|@|#|$]/);
+    if (reg.test(lines[parm.position.line][parm.position.character])) {
+        connection.console.info(`Matched`);
+        let left = parm.position.character;
+        let right = parm.position.character;
+
+        while(left > 0) {
+            connection.console.info(`Left is ${left} - ${lines[parm.position.line][left]}`);
+            left--;
+            if (!reg.test(lines[parm.position.line][left])) {
+                break;
+            }
+        }
+        while(right < lines[parm.position.line].length) {
+            connection.console.info(`Right is ${right} - ${lines[parm.position.line][right]}`);
+            right++;
+            if (!reg.test(lines[parm.position.line][right])) {
+                break;
+            }
+        }
+
+        const symb = lines[parm.position.line].substring(left + 1, right);
+        connection.console.info(`Left is ${left} and right is ${right}, '${symb}'`);
+        return symbolsCache.get(symb);
+    } else {
+        connection.console.info(`Did not match valid symbol`);
+        return;
+    }
+
+    // for (let i = 0; i < lines.length - 1; i++) {
+
+    //     // if space or * in column one, it's not a symbol
+    //     if (lines[i][0] !== " " && lines[i][0] !== "*") {
+
+    //         // compress multiple spaces to a single space
+    //         let tokenizedLine = lines[i].replace(/\s+/g, " ").split(" ");
+
+    //         // remove blank entries
+    //         tokenizedLine = tokenizedLine.filter((entry) => entry !== "");
+
+    //         if (tokenizedLine.length > 0) {
+
+    //             let kind: SymbolKind = SymbolKind.Constant;
+    //             if (tokenizedLine[1]) {
+    //                 if (tokenizedLine[1] === "DSECT") {
+    //                     kind = SymbolKind.Object;
+    //                 }
+    //             }
+
+    //             const entry: Location = {
+    //                 uri: parm.textDocument.uri,
+    //                 range: {
+    //                     start: { line: i, character: 0 },
+    //                     end: { line: i, character: tokenizedLine[0].length - 1 }
+    //                 }
+    //             };
+
+    //             symbols.push(entry);
+    //         }
+
+    //     }
+    // }
+    // return symbols;
+});
+
+
+// TODO(Kelosky): workspace symbols
 connection.onDocumentSymbol((parm) => {
+
+    connection.console.info(`onDocumentSymbol invoked ${JSON.stringify(parm, null, 2)}`);
 
     const symbols: SymbolInformation[] = [];
     const document = documents.get(parm.textDocument.uri);
     if (!document) {
         return null;
     }
+
+    connection.console.info(`Clearing symbol cache...`);
+    symbolsCache.clear();
 
     const lines = document.getText().split("\n");
     for (let i = 0; i < lines.length - 1; i++) {
@@ -218,7 +262,7 @@ connection.onDocumentSymbol((parm) => {
         if (lines[i][0] !== " " && lines[i][0] !== "*") {
 
             // compress multiple spaces to a single space
-            let  tokenizedLine = lines[i].replace(/\s+/g, " ").split(" ");
+            let tokenizedLine = lines[i].replace(/\s+/g, " ").split(" ");
 
             // remove blank entries
             tokenizedLine = tokenizedLine.filter((entry) => entry !== "");
@@ -227,12 +271,14 @@ connection.onDocumentSymbol((parm) => {
 
                 let kind: SymbolKind = SymbolKind.Constant;
 
+                // TODO(Kelosky): put all DSECT items under the object
                 if (tokenizedLine[1]) {
                     if (tokenizedLine[1] === "DSECT") {
                         kind = SymbolKind.Object;
                     }
                 }
 
+                // TODO(Kelosky): put all ORG items in groups
                 const entry: SymbolInformation = {
                     name: tokenizedLine[0],
                     kind,
@@ -246,6 +292,9 @@ connection.onDocumentSymbol((parm) => {
                 };
 
                 symbols.push(entry);
+
+                // Node(Kelosky): if this performs poorly, consider symbols being created
+                symbolsCache.set(entry.name, [entry.location]);
             }
 
         }
@@ -253,9 +302,6 @@ connection.onDocumentSymbol((parm) => {
     return symbols;
 });
 
-// Make the text document manager listen on the connection
-// for open, change and close text document events
 documents.listen(connection);
 
-// Listen on the connection
 connection.listen();
